0a13fe630cb3f1f8c997b80f9dc3d913c52c83e1
Onkar Malawade <onkarmalawade@gmail.com>
2025-04-22 16:46:10 +0530

    structure

--- library-mgt-sys/src/app.module.ts
+++ library-mgt-sys/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { MembersModule } from './members/members.module';
+import { BorrowModule } from './borrow/borrow.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule, MembersModule, BorrowModule],
   controllers: [AppController],
   providers: [AppService],
 })
